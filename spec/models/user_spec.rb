e34124db2ead216638288287fd20e5ddbcd54e63
Julius Pranevicius <julius.pranevicius@gmail.com>
2012-11-19 19:27:23 +0100

    Improve user model, tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,6 +62,16 @@ describe User do
 		end
 	end
 
+	describe "when email address with mixed case" do
+		let(:mixed_case_email) { @user.email = "mIcHaEl@HARtl.com" }
+
+		it "should be saved as lower case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "when email address is already taken" do
 		before do
 			user_with_same_email = @user.dup
