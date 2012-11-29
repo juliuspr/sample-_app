038d1527ca1df606f5bd9d4712d6c9a657399b5a
Julius Pranevicius <julius.pranevicius@gmail.com>
2012-11-29 21:27:33 +0100

    working update user profile (buggy)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,7 +44,7 @@ describe "User Pages" do
   			fill_in "Name", with: "Example User"
   			fill_in "Email", with: "example@tmail.com"
   			fill_in "Password", with: "foobar"
-  			fill_in "Confirmation", with: "foobar"
+  			fill_in "Password confirmation", with: "foobar"
   		end
 
   		it "should create a new user" do
@@ -60,7 +60,43 @@ describe "User Pages" do
   		end
 
   	end
+  end # end signup
 
-  end
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
 
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: "http://gravatar.com/emails") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New name" }
+      let(:new_email) { "email@dot.com" }
+
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Password confirmation", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('h1', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+
+  end #end edit
 end
