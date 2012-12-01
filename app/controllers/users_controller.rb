0f8da60d2060b018fec3f72ea6da801c3de5a7b2
Julius Pranevicius <julius.pranevicius@gmail.com>
2012-12-01 21:49:48 +0100

    Authorization for restricted pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
 
 
 	def show
@@ -22,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # fix success
       flash[:success] = "Profile updated!"
@@ -36,5 +36,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in" 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
